39f38a7b1249069f4fa0917ff433c125b2980e8e
Khoale38 <kevinle38@gmail.com>
2022-10-02 12:40:35 +0700

    [UPDATE] done base UI of collection

--- src/component/Home/collection.tsx
+++ src/component/Home/collection.tsx
@@ -3,13 +3,13 @@ import popcat from "../../asset/popcat.jpg";
 import "../../styles/pages/Home/collection.scss";
 const Collection: React.FC = () => {
   return (
-    <div className="card text-center">
+    <div className="card ">
       <img src={popcat} className="card-img-top collection-img" alt="..." />
-      <div className="card-body px-3 py-2 ">
+      <div className="card-body card-cbody px-3 py-0 d-flex flex-row align-items-center">
         <div className="square">
           <img />
         </div>
-        <div className="card-title collection-name">Card title</div>
+        <div className="card-title collection-name m-0">Card title</div>
       </div>
     </div>
   );
