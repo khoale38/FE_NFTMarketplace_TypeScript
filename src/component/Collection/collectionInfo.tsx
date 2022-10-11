4b4e652de0cfb3c1b265c3299821ccaeed011528
khoale38 <kevinle38@gmail.com>
2022-10-11 13:49:48 +0700

    add asset

---
+++ src/component/Collection/collectionInfo.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import star from "../../asset/star.svg";
+import share from "../../asset/share.svg";
+import more from "../../asset/more.svg";
+const collectionInfo: React.FC = () => {
+  return (
+    <div className=" collection-info-grid ">
+      <div className="col ">
+        <div className="collection-name">Collection Name</div>
+        <span className="by">
+          By <b className="author-name">Author Name</b>
+        </span>
+        <div className="collection-describe">
+          Description for NFT collection. Example this is one.
+        </div>
+      </div>
+      <div className=" ">
+        <div className="col collection-interact ">
+          <img src={star} />
+          <img src={share} className="px-3" />
+          <img src ={more}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default collectionInfo;
