b3a777a5495c86650176b3b6effba8a84f782260
Khoale38 <kevinle38@gmail.com>
2022-10-24 21:40:01 +0700

    add price filter

--- src/component/Collection/CollectionFilter/colllectionPrice.tsx
+++ src/component/Collection/CollectionFilter/colllectionPrice.tsx
@@ -4,12 +4,52 @@ const colllectionPrice = () => {
   return (
     <div>
       Price
-      <div className="custom-dropdown">
-        <span>Mouse over me</span>
-        <div className="custom-dropdown-content">
-          <p>Hello World!</p>
+      <div className="d-flex">
+        <div className="dropdown ">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            USD
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                USD
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                ETH
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Min"
+            aria-label="Min"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+        <div>To</div>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Max"
+            aria-label="Max"
+            aria-describedby="basic-addon1"
+          />
         </div>
       </div>
+      <button type="button" className="btn btn-primary container-fluid">
+        Primary
+      </button>
     </div>
   );
 };
