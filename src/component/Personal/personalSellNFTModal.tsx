23d51652fa1e9419e99c19ff40cc96270472277b
khoale38 <kevinle38@gmail.com>
2022-12-17 22:29:04 +0700

    done csss

--- src/component/Personal/personalSellNFTModal.tsx
+++ src/component/Personal/personalSellNFTModal.tsx
@@ -42,53 +42,60 @@ const personalSellNFTModal = (props: any) => {
                 alt="avatar"
                 className="personal-sell-modal-image me-3 col-lg"
               />
-              <div className=" col-lg">
+              <div className=" col-lg mt-3 mt-md-0">
                 <div className="input-group mb-3 personal-sell-modal-input">
                   <input
                     type="text"
                     placeholder="Input a price"
-                    className="form-control"
-                    aria-label="Text input with dropdown button"
+                    className="form-control personal-sell-modal-text personal-modal-textfield "
+
                   />
-                  <button
-                    className="btn btn-outline-secondary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    ETH
-                  </button>
-                  <ul className="dropdown-menu dropdown-menu-end">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        ETH
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        WETH
-                      </a>
-                    </li>
-                  </ul>
+                  <div className="dropdown-center personal-sell-modal-text">
+                    <button
+                      className="btn btn-outline-secondary dropdown-toggle  personal-NFT-more-dropdown-open"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      ETH
+                    </button>
+                    <ul className="dropdown-menu dropdown-menu-end personal-NFT-more-dropdown-menu">
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          ETH
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          WETH
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
                 <div>
-                  Summary
-                  <div className="mt-3">
-                    <div className="d-flex container-fluid justify-content-between">
-                      <div>Listing Fee</div>
+                  <div className="personal-sell-modal-text personal-sell-modal-bold ">
+                    {" "}
+                    Summary
+                  </div>
+                  <div className="mt-2">
+                    <div className="d-flex container-fluid justify-content-between personal-sell-modal-text">
+                      <div className=" ">Listing Fee</div>
                       <div>12 ETH</div>
                     </div>
-                    <div className="d-flex container-fluid justify-content-between">
+                    <div className="d-flex container-fluid justify-content-between personal-sell-modal-text">
                       <div>Service Fee</div>
                       <div>2.5 %</div>
                     </div>
-                    <div className="d-flex container-fluid justify-content-between">
+                    <div className="d-flex container-fluid justify-content-between personal-sell-modal-text">
                       <div>Creator Fee</div>
                       <div>0 %</div>
                     </div>
                     <hr className="personal-sell-modal-divider container-fluid mt-2" />
-                    <div className="d-flex container-fluid justify-content-between p-0">
-                      <div>Potential Earning</div>
+                    <div className="d-flex container-fluid justify-content-between p-0 personal-sell-modal-text align-items-center">
+                      <div className="personal-sell-modal-bold">
+                        Total Earning
+                      </div>
                       <div>12 ETH</div>
                     </div>
                   </div>
