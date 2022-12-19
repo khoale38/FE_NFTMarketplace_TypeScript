4183d8beb8f5655be53d89ebb821451d30ba6dd8
khoale38 <kevinle38@gmail.com>
2022-12-19 21:27:37 +0700

    setup axios for api

--- src/service/axios.tsx
+++ src/service/axios.tsx
@@ -1,1 +1,9 @@
-export {}
+import axios from "axios";
+
+export default axios.create({
+  timeout: 2000,
+  baseURL: "URL go here", //TODO: HTTP Go Here
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+});
