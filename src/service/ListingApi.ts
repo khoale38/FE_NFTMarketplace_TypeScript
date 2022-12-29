eac9161f0ec65b6bbfcd7fddd4c02722ed63de1d
Duong Hoang Long <longdh210@gmail.com>
2022-12-29 22:08:29 +0700

    update listing

--- src/service/ListingApi.ts
+++ src/service/ListingApi.ts
@@ -4,15 +4,20 @@ import { WalletNFT } from "model/NFT";
 import axios from "./axios";
 
 class ListingNFT {
-    listingNFT(makerAddress: string, tokenId: number, target: string, basePrice: number, paymentToken: string, listingTime: number, expirationTime: number): Promise<WalletNFT> {
-        return axios.post(`/contract/listing`, {
+    listingNFT(makerAddress: string, taker: string, feeRecipient: string, _calldata: string, tokenId: number, target: string, basePrice: number, paymentToken: string, listingTime: number, expirationTime: number, sellSig: string, salt: number): Promise<any> {
+        return axios.post(`/marketplace/listing`, {
             makerAddress,
+            taker,
+            feeRecipient,
+            _calldata,
             tokenId,
             target,
             basePrice,
             paymentToken,
             listingTime,
-            expirationTime
+            expirationTime,
+            sellSig,
+            salt
         });
     }
 }
--- src/service/axios.tsx
+++ src/service/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   timeout: 2000,
-  baseURL: "http://ec2-54-249-223-125.ap-northeast-1.compute.amazonaws.com:3000/api/v1/", //TODO: HTTP Go Here
+  baseURL: "http://localhost:3000/api/v1/", //TODO: HTTP Go Here
   headers: {
     "Content-Type": "application/json",
   },
