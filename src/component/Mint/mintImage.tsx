d874a1f75dc9492f1a19b7a75e81075681d86561
Duong Hoang Long <longdh210@gmail.com>
2023-01-04 17:17:24 +0700

    update mint feature

--- src/component/Mint/mintImage.tsx
+++ src/component/Mint/mintImage.tsx
@@ -2,9 +2,31 @@ import React, { useEffect, useRef, useState } from "react";
 import "../../styles/pages/Mint/mintImage.scss";
 import media from "../../asset/media.svg";
 import close from "../../asset/close2.svg";
-const MintImage = () => {
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+import { Buffer } from "buffer";
+
+const projectId = "2Jr08tvGRsfLQIYPcCB3DDYT8OF";
+const projectSecret = "a1aed34009ac37fe79ca67d5a63e7d57";
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+const clientInfura = ipfsHttpClient({
+  // url: "https://ipfs.infura.io:5001", 
+  host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    },
+});
+
+type Props = {
+  mintImageToBody: (url: string) => void;
+};
+
+const MintImage: React.FC<Props> = ({ mintImageToBody }) => {
   const [selectedFile, setSelectedFile] = useState<any | null>();
   const [preview, setPreview] = useState<string>();
+  const [fileUrl, setFileUrl] = React.useState("");
   const inputFile = useRef<any>(null);
   const onButtonClick = () => {
     // `current` points to the mounted file input element
@@ -23,15 +45,34 @@ const MintImage = () => {
     return () => URL.revokeObjectURL(objectUrl);
   }, [selectedFile]);
 
-  const onSelectFile = (e: any) => {
-    if (!e.target.files || e.target.files.length === 0) {
-      setSelectedFile(undefined);
-      return;
-    }
+  // const onSelectFile = (e: any) => {
+  //   if (!e.target.files || e.target.files.length === 0) {
+  //     setSelectedFile(undefined);
+  //     return;
+  //   }
 
-    // I've kept this example simple by using the first image instead of multiple
-    setSelectedFile(e.target.files[0]);
-  };
+  //   // I've kept this example simple by using the first image instead of multiple
+  //   setSelectedFile(e.target.files[0]);
+  // };
+  const onChange = async (e: any) => {
+    const file = e.target.files[0]
+    try {
+      const added = await clientInfura.add(
+          file,
+          {
+              progress: (prog) => console.log(`received: ${prog}`)
+          }
+      )
+      const url = `https://ipfs.infura.io:5001/ipfs/${added.path}`;
+      console.log(url);
+      setFileUrl(url);
+      mintImageToBody(url);
+    }
+    catch(error) {
+        console.log('Error uploading file: ', error)
+    }
+  }
+
   return (
     <div className='my-3'>
       <div className="mint-text-style mint-black-text mint-heading2 ">
@@ -43,7 +84,7 @@ const MintImage = () => {
       </div>
       <input
         type="file"
-        onChange={onSelectFile}
+        onChange={onChange}
         ref={inputFile}
         style={{ display: "none" }}
       />
